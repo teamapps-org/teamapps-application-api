02e61cc05b7f4578a946ebee1bf5961397cc931b
Matthias Bernstein <matthias.bernstein@teamapps.org>
2021-07-04 15:31:04 +0200

    application instance data methods as interface with default implementation

--- src/main/java/org/teamapps/application/api/application/perspective/AbstractApplicationPerspective.java
+++ src/main/java/org/teamapps/application/api/application/perspective/AbstractApplicationPerspective.java
@@ -20,6 +20,7 @@
 package org.teamapps.application.api.application.perspective;
 
 import org.teamapps.application.api.application.ApplicationInstanceData;
+import org.teamapps.application.api.application.ApplicationInstanceDataMethods;
 import org.teamapps.application.api.config.ApplicationConfig;
 import org.teamapps.application.api.desktop.ApplicationDesktop;
 import org.teamapps.application.api.privilege.*;
@@ -37,7 +38,7 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-public abstract class AbstractApplicationPerspective implements ApplicationPerspective {
+public abstract class AbstractApplicationPerspective implements ApplicationPerspective, ApplicationInstanceDataMethods {
 
 	public final Event<Void> onPerspectiveInitialized = new Event<>();
 	public final Event<Void> onPerspectiveRefreshRequested = new Event<>();
@@ -81,38 +82,6 @@ public abstract class AbstractApplicationPerspective implements ApplicationPersp
 		return perspective;
 	}
 
-	public void showPerspective(Perspective perspective) {
-		applicationInstanceData.showPerspective(perspective);
-	}
-
-	public void writeActivityLog(String title, String data) {
-		getApplicationInstanceData().writeActivityLog(title, data);
-	}
-
-	public void writeExceptionLog(String title, Throwable throwable) {
-		getApplicationInstanceData().writeExceptionLog(title, throwable);
-	}
-
-	public SessionUser getUser() {
-		return applicationInstanceData.getUser();
-	}
-
-	public MultiProgressDisplay getMultiProgressDisplay() {
-		return applicationInstanceData.getMultiProgressDisplay();
-	}
-
-	public <RESULT> void runTaskAsync(Icon icon, String title, Supplier<RESULT> task, Consumer<RESULT> uiResultTask) {
-		applicationInstanceData.runTaskAsync(icon, title, task, uiResultTask);
-	}
-
-	public ApplicationDesktop createApplicationDesktop() {
-		return applicationInstanceData.createApplicationDesktop();
-	}
-
-	public ApplicationConfig<?> getApplicationConfig() {
-		return applicationInstanceData.getApplicationConfig();
-	}
-
 	public ApplicationInstanceData getApplicationInstanceData() {
 		return applicationInstanceData;
 	}
@@ -125,52 +94,6 @@ public abstract class AbstractApplicationPerspective implements ApplicationPersp
 		return getLocalized(key, (Object) null);
 	}
 
-	public String getLocalized(TranslatableText translatableText) {
-		return applicationInstanceData.getLocalized(translatableText);
-	}
 
-	public String getLocalized(String key, Object... parameters) {
-		return applicationInstanceData.getLocalized(key, parameters);
-	}
-
-	public boolean isAllowed(SimplePrivilege simplePrivilege) {
-		return applicationInstanceData.isAllowed(simplePrivilege);
-	}
-
-	public boolean isAllowed(SimpleOrganizationalPrivilege group, OrganizationUnitView OrganizationUnitView) {
-		return applicationInstanceData.isAllowed(group, OrganizationUnitView);
-	}
-
-	public boolean isAllowed(SimpleCustomObjectPrivilege group, PrivilegeObject privilegeObject) {
-		return applicationInstanceData.isAllowed(group, privilegeObject);
-	}
-
-	public boolean isAllowed(StandardPrivilegeGroup group, Privilege privilege) {
-		return applicationInstanceData.isAllowed(group, privilege);
-	}
-
-	public boolean isAllowed(OrganizationalPrivilegeGroup group, Privilege privilege, OrganizationUnitView OrganizationUnitView) {
-		return applicationInstanceData.isAllowed(group, privilege, OrganizationUnitView);
-	}
-
-	public boolean isAllowed(CustomObjectPrivilegeGroup group, Privilege privilege, PrivilegeObject privilegeObject) {
-		return applicationInstanceData.isAllowed(group, privilege, privilegeObject);
-	}
-
-	public List<OrganizationUnitView> getAllowedUnits(SimpleOrganizationalPrivilege simplePrivilege) {
-		return applicationInstanceData.getAllowedUnits(simplePrivilege);
-	}
-
-	public List<OrganizationUnitView> getAllowedUnits(OrganizationalPrivilegeGroup group, Privilege privilege) {
-		return applicationInstanceData.getAllowedUnits(group, privilege);
-	}
-
-	public List<PrivilegeObject> getAllowedPrivilegeObjects(SimpleCustomObjectPrivilege simplePrivilege) {
-		return applicationInstanceData.getAllowedPrivilegeObjects(simplePrivilege);
-	}
-
-	public List<PrivilegeObject> getAllowedPrivilegeObjects(CustomObjectPrivilegeGroup group, Privilege privilege) {
-		return applicationInstanceData.getAllowedPrivilegeObjects(group, privilege);
-	}
 
 }
