f7716fda4935cfbd91e8decabd3d6121009832a4
Matthias Bernstein <matthias.bernstein@teamapps.org>
2021-07-08 10:43:11 +0200

    key compare with merge function to allow duplicate keys

--- src/main/java/org/teamapps/application/tools/KeyCompare.java
+++ src/main/java/org/teamapps/application/tools/KeyCompare.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,11 +19,20 @@
  */
 package org.teamapps.application.tools;
 
-import java.util.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class KeyCompare<A, B> {
+	private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
 	private final Collection<A> aCollection;
 	private final Collection<B> bCollection;
@@ -35,18 +44,29 @@ public class KeyCompare<A, B> {
 	private final List<B> inA = new ArrayList<>();
 	private Map<String, A> aByKey;
 	private Map<String, B> bByKey;
+	private final boolean enforceUniqueKeys;
 
 	public KeyCompare(Collection<A> aCollection, Collection<B> bCollection, Function<A, String> keyOfA, Function<B, String> keyOfB) {
+		this(aCollection, bCollection, keyOfA, keyOfB, false);
+	}
+
+	public KeyCompare(Collection<A> aCollection, Collection<B> bCollection, Function<A, String> keyOfA, Function<B, String> keyOfB, boolean enforceUniqueKeys) {
 		this.aCollection = aCollection;
 		this.bCollection = bCollection;
 		this.keyOfA = keyOfA;
 		this.keyOfB = keyOfB;
+		this.enforceUniqueKeys = enforceUniqueKeys;
 		compare();
 	}
 
 	private void compare() {
-		aByKey = aCollection.stream().collect(Collectors.toMap(keyOfA, a -> a));
-		bByKey = bCollection.stream().collect(Collectors.toMap(keyOfB, b -> b));
+		if (enforceUniqueKeys) {
+			aByKey = aCollection.stream().collect(Collectors.toMap(keyOfA, a -> a));
+			bByKey = bCollection.stream().collect(Collectors.toMap(keyOfB, b -> b));
+		} else {
+			aByKey = aCollection.stream().collect(Collectors.toMap(keyOfA, a -> a, getMergeFunctionA()));
+			bByKey = bCollection.stream().collect(Collectors.toMap(keyOfB, b -> b, getMergeFunctionB()));
+		}
 		for (A a : aCollection) {
 			if (bByKey.containsKey(keyOfA.apply(a))) {
 				inB.add(a);
@@ -61,6 +81,20 @@ public class KeyCompare<A, B> {
 				notInA.add(b);
 			}
 		}
+	}
+
+	private BinaryOperator<A> getMergeFunctionA() {
+		return (key1, key2) -> {
+			LOGGER.warn("KeyCompare-A with non-unique keys:" + keyOfA.apply(key1) + " (" + key1 + ", " + key2 + ")");
+			return key1;
+		};
+	}
+
+	private BinaryOperator<B> getMergeFunctionB() {
+		return (key1, key2) -> {
+			LOGGER.warn("KeyCompare-B with non-unique keys:" + keyOfB.apply(key1) + " (" + key1 + ", " + key2 + ")");
+			return key1;
+		};
 	}
 
 	public A getA(B b) {
