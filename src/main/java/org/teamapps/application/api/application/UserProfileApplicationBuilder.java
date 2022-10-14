fbefe0c34428b3d05e4a63f8b013f3fa5d8144f9
Matthias Bernstein <matthias.bernstein@teamapps.org>
2022-10-14 14:23:49 +0200

    user profiles app

---
+++ src/main/java/org/teamapps/application/api/application/UserProfileApplicationBuilder.java
@@ -0,0 +1,10 @@
+package org.teamapps.application.api.application;
+
+import org.teamapps.application.api.application.perspective.PerspectiveBuilder;
+
+import java.util.List;
+
+public interface UserProfileApplicationBuilder {
+
+	List<PerspectiveBuilder> getUserProfilePerspectiveBuilders();
+}
