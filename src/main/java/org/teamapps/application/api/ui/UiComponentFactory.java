d59495a6b93bf3bcb610d50b1ceefea3d2972007
Matthias Bernstein <matthias.bernstein@teamapps.org>
2024-06-05 16:55:25 +0200

    updated ui factory

--- src/main/java/org/teamapps/application/api/ui/UiComponentFactory.java
+++ src/main/java/org/teamapps/application/api/ui/UiComponentFactory.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,7 +19,6 @@
  */
 package org.teamapps.application.api.ui;
 
-import org.teamapps.application.ux.localize.TranslatableField;
 import org.teamapps.icons.Icon;
 import org.teamapps.model.controlcenter.OrganizationUnitTypeView;
 import org.teamapps.model.controlcenter.OrganizationUnitView;
@@ -36,6 +35,7 @@ import java.util.function.Supplier;
 public interface UiComponentFactory {
 
 	ComboBox<OrganizationUnitView> createOrganizationUnitComboBox(Supplier<Collection<OrganizationUnitView>> allowedUnitsSupplier);
+
 	ComboBox<OrganizationUnitView> createOrganizationUnitComboBox(Set<OrganizationUnitView> allowedUnits);
 
 	TagComboBox<OrganizationUnitTypeView> createOrganizationUnitTypeTagComboBox();
@@ -45,8 +45,6 @@ public interface UiComponentFactory {
 	TemplateField<Integer> createUserTemplateField();
 
 	ComboBox<UserView> createUserSelectionComboBox();
-
-	TranslatableField createTranslatableField();
 
 	default TranslationKeyField createTranslationKeyField(String linkButtonCaption) {
 		return createTranslationKeyField(linkButtonCaption, false, false);
