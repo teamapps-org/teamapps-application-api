42ac16431226835aa5afbcb0b58a43cb76c4230c
Matthias Bernstein <matthias.bernstein@teamapps.org>
2025-05-22 18:47:49 +0200

    updated utils

--- src/main/java/org/teamapps/application/ux/UiUtils.java
+++ src/main/java/org/teamapps/application/ux/UiUtils.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -94,6 +94,13 @@ public class UiUtils {
 		});
 		return templateField;
 	}
+
+	public static TemplateField<String> createBadgeField(Color textColor, Color backgroundColor, String value) {
+		TemplateField<String> badgeField = createBadgeField(textColor, backgroundColor);
+		badgeField.setValue(value);
+		return badgeField;
+	}
+
 
 	public static <TYPE> TemplateField<TYPE> createTemplateField(Template template, PropertyProvider<TYPE> propertyProvider) {
 		TemplateField<TYPE> templateField = new TemplateField<>(template);
