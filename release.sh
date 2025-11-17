78523ca6515faa4fb69726218fbeaafccf65a172
Matthias Bernstein <matthias.bernstein@teamapps.org>
2025-11-17 18:41:07 +0100

    update maven central plugin

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export JDK_JAVA_OPTIONS='--add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED --add-opens java.base/java.text=ALL-UNNAMED --add-opens java.desktop/java.awt.font=ALL-UNNAMED'
-mvn -e -P javadoc-jar,sources-jar,release release:prepare release:perform && cd target/checkout && mvn -X -P deploy central-publishing:publish
+mvn -e -P javadoc-jar,sources-jar,release release:prepare release:perform && cd target/checkout && mvn -X -P release central-publishing:publish
